d7e0d141e01c96c696a3596fe06a6000c18b5080
Alexey Kozlov <alumedot@icloud.com>
2020-04-13 20:26:11 +0300

    navigation subscription

--- src/screens/shop/ProductsOverviewScreen/ProductsOverviewScreen.tsx
+++ src/screens/shop/ProductsOverviewScreen/ProductsOverviewScreen.tsx
@@ -18,89 +18,99 @@ import { IProps } from './types';
 
 
 const ProductsOverviewScreen = (props: IProps) => {
-    const products = useSelector(({ products }: IRootReduxState) => products.availableProducts);
-    const isLoading = useSelector(({products}: IRootReduxState) => products.isLoading);
-    const dispatch = useDispatch();
+  const products = useSelector(({products}: IRootReduxState) => products.availableProducts);
+  const isLoading = useSelector(({products}: IRootReduxState) => products.isLoading);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(actionsProducts.getProducts());
-    }, [dispatch]);
+  useEffect(() => {
+    const willFocusSub = props.navigation.addListener('willFocus', () => {
+      dispatch(actionsProducts.getProducts());
+    })
 
-    const onSelectItem = (id: string, title: string) => {
-        props.navigation.navigate(
-            Screens.ProductDetail,
-            {
-                productId: id,
-                productTitle: title,
-            }
-        )
+    return () => {
+      willFocusSub.remove();
     };
+  }, [dispatch]);
 
-    if (isLoading) {
-        return (
-            <View style={styles.centered}>
-                <ActivityIndicator size="large" color={Colors.Primary} />
-            </View>
-        )
-    }
+  useEffect(() => {
+    dispatch(actionsProducts.getProducts());
+  }, [dispatch]);
 
-    if (!isLoading && !products.length) {
-        return (
-            <View style={styles.centered}>
-                <Text>No products found. Maybe start adding some!</Text>
-            </View>
-        );
-    }
-
-    return (
-        <FlatList
-            data={products}
-            keyExtractor={item => item.id}
-            renderItem={itemData => (
-                <ProductItem
-                    image={itemData.item.imageUrl}
-                    title={itemData.item.title}
-                    price={itemData.item.price}
-                    onSelect={() => onSelectItem(itemData.item.id, itemData.item.title)}
-                >
-                    <Button
-                        color={Colors.Primary}
-                        title="View details"
-                        onPress={() => onSelectItem(itemData.item.id, itemData.item.title)}
-                    />
-                    <Button
-                        color={Colors.Primary}
-                        title="To cart"
-                        onPress={() => dispatch(cartActions.addToCart(itemData.item))}
-                    />
-                </ProductItem>
-            )}
-        />
+  const onSelectItem = (id: string, title: string) => {
+    props.navigation.navigate(
+      Screens.ProductDetail,
+      {
+        productId: id,
+        productTitle: title,
+      }
     )
+  };
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.Primary}/>
+      </View>
+    )
+  }
+
+  if (!isLoading && !products.length) {
+    return (
+      <View style={styles.centered}>
+        <Text>No products found. Maybe start adding some!</Text>
+      </View>
+    );
+  }
+
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onSelect={() => onSelectItem(itemData.item.id, itemData.item.title)}
+        >
+          <Button
+            color={Colors.Primary}
+            title="View details"
+            onPress={() => onSelectItem(itemData.item.id, itemData.item.title)}
+          />
+          <Button
+            color={Colors.Primary}
+            title="To cart"
+            onPress={() => dispatch(cartActions.addToCart(itemData.item))}
+          />
+        </ProductItem>
+      )}
+    />
+  )
 };
 
 ProductsOverviewScreen.navigationOptions = (navData: NavigationStackScreenProps) => {
-    return {
-        headerTitle: 'All Products',
-        headerLeft: () => <HeaderButtonMenu navData={navData} />,
-        headerRight: () => (
-            <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                <Item
-                    title="Cart"
-                    iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
-                    onPress={() => navData.navigation.navigate(Screens.Cart)}
-                />
-            </HeaderButtons>
-        ),
-    }
+  return {
+    headerTitle: 'All Products',
+    headerLeft: () => <HeaderButtonMenu navData={navData}/>,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Cart"
+          iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+          onPress={() => navData.navigation.navigate(Screens.Cart)}
+        />
+      </HeaderButtons>
+    ),
+  }
 };
 
 const styles = StyleSheet.create({
-    centered: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
 
 export default ProductsOverviewScreen;
